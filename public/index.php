58c63190726407551d61068e8c8f189e8cdfacbc
hojinlee <infohojin@gmail.com>
2020-09-05 16:46:57 +0900

    autoload file check and error display

--- public/index.php
+++ public/index.php
@@ -1,16 +1,33 @@
 <?php
-$autoload = require "../vendor/autoload.php";
-\jiny\session_start();
+/**
+ * 컴포저 오토로드 로드 
+ */
+$autoload_path = "../vendor/autoload.php";
+if (file_exists($autoload_path)) {
+    $autoload = require "../vendor/autoload.php";
+    \jiny\session_start();
 
-$start = \jiny\core\bootstart();
-echo $start->main();
+    $start = \jiny\core\bootstart();
+    echo $start->main();
 
-// 컨트롤러 직접 실행방법
-// $boot->controller("membersRegist");
+    // 컨트롤러 직접 실행방법
+    // $boot->controller("membersRegist");
 
-// 컴포저 패키지 실행
-// $boot->package("\Jiny\Members\Registration");
-// echo $boot->package("\Jiny\Members\Login");
+    // 컴포저 패키지 실행
+    // $boot->package("\Jiny\Members\Registration");
+    // echo $boot->package("\Jiny\Members\Login");
+} else {
+    $msg = "오토로드 파일을 읽을 수 없어, 실행이 불가능 합니다.";
+    $filename = "../resource/error.html";
+    if (file_exists($filename)) {
+        $body = file_get_contents($filename);
+        echo str_replace("</body>", $msg."</body>", $body);
+    } else {
+        echo $msg;
+    }
+    exit;
+}
+
 
 
 
